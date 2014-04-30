a16af62a2798a93a98c57e42a611f9d18cbcd20d
Oliver Schleidt <oliverschleidt@gmail.com>
2014-04-30 13:40:07 +0200

    My initial commit message

---
+++ TestEins/Client/script.js
@@ -0,0 +1,13 @@
+﻿window.onload = function () {
+    var connection = $.hubConnection();
+    var testHub = connection.createHubProxy('testHub');
+    testHub.on('sendMessage', function (name, message) {
+        $('#chat').html($('#chat').html() + '<br/>' + name + ' schrieb: ' + message);
+    });
+
+    connection.start().done(function () {
+        $('#senden').click(function () {
+            testHub.invoke('sendMessage', $('#name').val(), $('#message').val());
+        });
+    });
+};
---
+++ TestEins/TestHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace TestEins
+{
+    public class TestHub : Hub
+    {
+        public TestHub() {
+
+        }
+
+        public void SendMessage(String sender, String message)
+        {
+            Clients.All.sendMessage(sender, message);
+        }
+    }
+}
